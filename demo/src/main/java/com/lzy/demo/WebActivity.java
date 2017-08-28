5822850a1afd5913ae500f7d3dcac07399df246b
wander <wangdou@gozap.com>
2017-08-28 12:54:28 +0800

    share

--- demo/src/main/java/com/lzy/demo/WebActivity.java
+++ demo/src/main/java/com/lzy/demo/WebActivity.java
@@ -18,7 +18,10 @@ package com.lzy.demo;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
@@ -40,12 +43,18 @@ import butterknife.Bind;
  */
 public class WebActivity extends BaseActivity {
 
-    public final static String URL = "url";
+    public final static String URL = "mUrl";
     public final static String TITLE = "title";
 
-    @Bind(R.id.toolbar) Toolbar toolbar;
-    @Bind(R.id.pb) ProgressBar pb;
-    @Bind(R.id.webView) WebView webView;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.pb)
+    ProgressBar pb;
+    @Bind(R.id.webView)
+    WebView webView;
+    @Bind(R.id.fab)
+    FloatingActionButton floatingActionButton;
+    private String mUrl;
 
     public static void runActivity(Context context, String title, String url) {
         Intent intent = new Intent(context, WebActivity.class);
@@ -58,7 +67,7 @@ public class WebActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
-        String url = getIntent().getStringExtra(URL);
+        mUrl = getIntent().getStringExtra(URL);
         String title = getIntent().getStringExtra(TITLE);
         initToolBar(toolbar, true, title);
 
@@ -79,9 +88,44 @@ public class WebActivity extends BaseActivity {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
+                mUrl = url;
                 return true;
             }
         });
-        webView.loadUrl(url);
+        webView.loadUrl(mUrl);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                share();
+            }
+        });
+    }
+
+    private void share() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, mUrl);
+        intent.setType("text/plain");
+        //设置分享列表的标题，并且每次都显示分享列表
+        startActivity(Intent.createChooser(intent, "分享到"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.web_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.share:
+                share();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
